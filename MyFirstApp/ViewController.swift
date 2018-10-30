e5872efe4b9957f5480778bc48a6fe4197634ebf
Ding-Chou Tsai <a-ding@caidingchoude-macbook-air.local>
2018-10-30 19:35:32 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 蔡丁丑 on 2018/10/30.
+//  Copyright © 2018年 YAO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
